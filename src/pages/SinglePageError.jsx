21ec2ad65d7c1a40f10a01a7a178fb4c06ca22a0
mathstutor19 <mathstutor192gmail.com>
2023-12-23 18:59:49 +0500

    first commit

---
+++ src/pages/SinglePageError.jsx
@@ -0,0 +1,7 @@
+import { useRouteError } from 'react-router-dom';
+
+const SinglePageError = () => {
+  const error = useRouteError();
+  return <h2>there was an error...</h2>;
+};
+export default SinglePageError;
